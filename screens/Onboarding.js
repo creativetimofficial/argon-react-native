f6a78f8675060840a0901a5d895ebb09cb84245b
zeeun <98653814+zeeun@users.noreply.github.com>
2024-01-16 13:51:45 +0900

    [feat]onboarding화면

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -4,9 +4,10 @@ import {
   Image,
   StyleSheet,
   StatusBar,
-  Dimensions
+  Dimensions,
 } from "react-native";
 import { Block, Button, Text, theme } from "galio-framework";
+import axios from "axios";
 
 const { height, width } = Dimensions.get("screen");
 
@@ -21,43 +22,56 @@ class Onboarding extends React.Component {
       <Block flex style={styles.container}>
         <StatusBar hidden />
         <Block flex center>
-        <ImageBackground
-            source={Images.Onboarding}
+          <ImageBackground
+            // source={Images.Onboarding}
             style={{ height, width, zIndex: 1 }}
           />
         </Block>
         <Block center>
-          <Image source={Images.LogoOnboarding} style={styles.logo} />
+          <Image
+            source={require("../assets/imgs/argon-logo-onboarding.png")}
+            style={styles.logo}
+          />
         </Block>
         <Block flex space="between" style={styles.padded}>
-            <Block flex space="around" style={{ zIndex: 2 }}>
-              <Block style={styles.title}>
-                <Block>
-                  <Text color="white" size={60}>
-                    Design
-                  </Text>
-                </Block>
-                <Block>
-                  <Text color="white" size={60}>
-                    System
-                  </Text>
-                </Block>
-                <Block style={styles.subTitle}>
-                  <Text color="white" size={16}>
-                    Fully coded React Native components.
-                  </Text>
-                </Block>
+          <Block flex space="around" style={{ zIndex: 2 }}>
+            <Block style={styles.title}>
+              <Block>
+                <Text color="white" size={60}>
+                  My own
+                </Text>
               </Block>
-              <Block center>
-                <Button
-                  style={styles.button}
-                  color={argonTheme.COLORS.SECONDARY}
-                  onPress={() => navigation.navigate("App")}
-                  textStyle={{ color: argonTheme.COLORS.BLACK }}
-                >
-                  Get Started
-                </Button>
+              <Block>
+                <Text color="white" size={60}>
+                  Pharmasist
+                </Text>
               </Block>
+              <Block style={styles.subTitle}>
+                <Text color="white" size={16}>
+                  나만의 단골약사로 약물의 위험에서 벗어나세요
+                </Text>
+              </Block>
+            </Block>
+            <Block center>
+              <Button
+                style={styles.button}
+                color={"yellow"}
+                onPress={() => {
+                  navigation.navigate("App");
+                  axios
+                    .post("/login/kakao")
+                    .then((response) => {
+                      console.log(response);
+                    })
+                    .catch((error) => {
+                      console.error(error);
+                    });
+                }}
+                textStyle={{ color: argonTheme.COLORS.BLACK }}
+              >
+                Kakao Login
+              </Button>
+            </Block>
           </Block>
         </Block>
       </Block>
@@ -67,7 +81,7 @@ class Onboarding extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.COLORS.BLACK
+    backgroundColor: theme.COLORS.BLACK,
   },
   padded: {
     paddingHorizontal: theme.SIZES.BASE * 2,
@@ -79,21 +93,21 @@ const styles = StyleSheet.create({
     width: width - theme.SIZES.BASE * 4,
     height: theme.SIZES.BASE * 3,
     shadowRadius: 0,
-    shadowOpacity: 0
+    shadowOpacity: 0,
   },
   logo: {
     width: 200,
     height: 60,
     zIndex: 2,
-    position: 'relative',
-    marginTop: '-50%'
+    position: "relative",
+    marginTop: "-50%",
   },
   title: {
-    marginTop:'-5%'
+    marginTop: "-5%",
   },
   subTitle: {
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
 });
 
 export default Onboarding;

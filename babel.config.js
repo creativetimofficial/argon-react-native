08096b567864678d7726d28fb764272ba708e3fe
zeeun <98653814+zeeun@users.noreply.github.com>
2024-01-24 14:57:41 +0900

    [fix]오류해결

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = function (api) {
           extensions: [".tsx", ".ts", ".js", ".json"],
         },
       ],
-      "react-native-reanimated/plugin",
+     ["react-native-reanimated/plugin"],
     ],
   };
 };

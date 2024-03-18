c3b64888120532a03e7e82a614a1b34d41ff4327
zeeun <98653814+zeeun@users.noreply.github.com>
2024-03-18 17:43:35 +0900

    [feat]디스크립션, 경고 완성

--- components/DrugRecordCard.js
+++ components/DrugRecordCard.js
@@ -1,26 +1,36 @@
-import React from "react";
-import {TouchableOpacity, View, Text, Image, StyleSheet } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  Dimensions,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import DefaultImage from "../assets/imgs/defaultpill.jpg"; // Adjust the path as necessary
 
+const { width } = Dimensions.get("screen");
 const DrugRecordCard = ({ item }) => {
   const navigation = useNavigation();
-  const { itemName, dailyFrequency, startDate, endDate, image, typeName } =
-    item;
+  const [currentImageUri, setCurrentImageUri] = useState(item.image);
+  const { itemName, dailyFrequency, startDate, endDate, typeName } = item;
 
   return (
     <TouchableOpacity
       style={styles.card}
       onPress={() =>
-        navigation.navigate("MedicineDetail", { itemName: item.itemName })
+        navigation.navigate("MedicineDetail", { itemName: itemName })
       }
     >
       <View style={styles.contentContainer}>
-        {image && (
-          <View style={styles.imageContainer}>
-            <Image source={{ uri: image }} style={styles.image} />
-          </View>
-        )}
+        <View style={styles.imageContainer}>
+          <Image
+            source={currentImageUri ? { uri: currentImageUri } : DefaultImage} // 이미지 URI가 있다면 네트워크 이미지를 사용하고, 그렇지 않으면 기본 이미지를 사용합니다.
+            style={styles.image}
+            onError={() => setCurrentImageUri(null)} // 이미지 로딩에 에러가 발생하면 기본 이미지로 변경합니다.
+          />
+        </View>
         <View style={styles.textContainer}>
           <Text style={styles.title}>이름: {itemName}</Text>
           <Text style={styles.text}>일일 투여 횟수: {dailyFrequency}</Text>
